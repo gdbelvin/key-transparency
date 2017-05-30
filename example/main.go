c13ad65c3f8634c7a4b47cdea59724cc065c48b6
Gary Belvin <gdb@google.com>
2017-05-30 15:18:34 -0500

    Add trillian log verifier

--- example/main.go
+++ example/main.go
@@ -7,26 +7,39 @@ import (
 	"github.com/google/keytransparency/core/client/kt"
 	"github.com/google/keytransparency/core/crypto/vrf"
 	"github.com/google/keytransparency/core/crypto/vrf/p256"
-	"github.com/google/keytransparency/core/fake"
 	tpb "github.com/google/keytransparency/core/proto/keytransparency_v1_types"
 	"github.com/google/keytransparency/core/tree/sparse"
 	tv "github.com/google/keytransparency/core/tree/sparse/verifier"
 	"github.com/google/trillian"
+	"github.com/google/trillian/client"
 	"github.com/google/trillian/crypto/keys"
+	"github.com/google/trillian/merkle/objhasher"
 	"github.com/gopherjs/gopherjs/js"
 	"golang.org/x/net/context"
 )
 
-func staticVRF() (vrf.PrivateKey, vrf.PublicKey, error) {
-	priv := `-----BEGIN EC PRIVATE KEY-----
+const (
+	sigPriv = `-----BEGIN EC PRIVATE KEY-----
 MHcCAQEEIHgSC8WzQK0bxSmfJWUeMP5GdndqUw8zS1dCHQ+3otj/oAoGCCqGSM49
 AwEHoUQDQgAE5AV2WCmStBt4N2Dx+7BrycJFbxhWf5JqSoyp0uiL8LeNYyj5vgkl
 K8pLcyDbRqch9Az8jXVAmcBAkvaSrLW8wQ==
 -----END EC PRIVATE KEY-----`
-	pub := `-----BEGIN PUBLIC KEY-----
+	sigPub = `-----BEGIN PUBLIC KEY-----
 MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE5AV2WCmStBt4N2Dx+7BrycJFbxhW
 f5JqSoyp0uiL8LeNYyj5vgklK8pLcyDbRqch9Az8jXVAmcBAkvaSrLW8wQ==
 -----END PUBLIC KEY-----`
+	priv = `-----BEGIN EC PRIVATE KEY-----
+MHcCAQEEIHgSC8WzQK0bxSmfJWUeMP5GdndqUw8zS1dCHQ+3otj/oAoGCCqGSM49
+AwEHoUQDQgAE5AV2WCmStBt4N2Dx+7BrycJFbxhWf5JqSoyp0uiL8LeNYyj5vgkl
+K8pLcyDbRqch9Az8jXVAmcBAkvaSrLW8wQ==
+-----END EC PRIVATE KEY-----`
+	pub = `-----BEGIN PUBLIC KEY-----
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE5AV2WCmStBt4N2Dx+7BrycJFbxhW
+f5JqSoyp0uiL8LeNYyj5vgklK8pLcyDbRqch9Az8jXVAmcBAkvaSrLW8wQ==
+-----END PUBLIC KEY-----`
+)
+
+func staticVRF() (vrf.PrivateKey, vrf.PublicKey, error) {
 	vrf, err := p256.NewVRFSignerFromPEM([]byte(priv))
 	if err != nil {
 		return nil, nil, err
@@ -39,15 +52,6 @@ f5JqSoyp0uiL8LeNYyj5vgklK8pLcyDbRqch9Az8jXVAmcBAkvaSrLW8wQ==
 }
 
 func staticKeyPair() (crypto.Signer, crypto.PublicKey, error) {
-	sigPriv := `-----BEGIN EC PRIVATE KEY-----
-MHcCAQEEIHgSC8WzQK0bxSmfJWUeMP5GdndqUw8zS1dCHQ+3otj/oAoGCCqGSM49
-AwEHoUQDQgAE5AV2WCmStBt4N2Dx+7BrycJFbxhWf5JqSoyp0uiL8LeNYyj5vgkl
-K8pLcyDbRqch9Az8jXVAmcBAkvaSrLW8wQ==
------END EC PRIVATE KEY-----`
-	sigPub := `-----BEGIN PUBLIC KEY-----
-MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE5AV2WCmStBt4N2Dx+7BrycJFbxhW
-f5JqSoyp0uiL8LeNYyj5vgklK8pLcyDbRqch9Az8jXVAmcBAkvaSrLW8wQ==
------END PUBLIC KEY-----`
 	sig, err := keys.NewFromPrivatePEM(sigPriv, "")
 	if err != nil {
 		return nil, nil, err
@@ -71,8 +75,8 @@ func main() {
 		log.Fatalf("Failed to load signing keypair: %v", err)
 	}
 
-	kt := kt.New(vrfPub, tv.New(0, sparse.CONIKSHasher), verifier,
-		fake.NewFakeTrillianLogVerifier())
+	tlog := client.NewLogVerifier(objhasher.ObjectHasher, verifier)
+	kt := kt.New(vrfPub, tv.New(0, sparse.CONIKSHasher), verifier, tlog)
 
 	trusted := trillian.SignedLogRoot{}
 	resp := &tpb.GetEntryResponse{}
